587a92525312e272615d8badb30b4f04c1821d74
Jebakani <vjishwarya@gmail.com>
2021-06-29 12:17:47 +0530

    [JEBAKANI] UC3-Check whether snake or Ladder

--- SnakeAndLadder/Program.cs
+++ SnakeAndLadder/Program.cs
@@ -4,12 +4,21 @@ namespace SnakeAndLadder
 {
     class Program
     {
+
         /// <summary>
         /// Snake and Ladder computation
         /// UC1-Initialize player and Initial position
         /// UC2- Generating Die Roll using Random class
+        /// UC3- Checking whether to move forward backward based on player choice
         /// </summary>
         /// <param name="args">The arguments.</param>
+        
+
+        const int LADDER = 2;
+        const int SNAKE = 1;
+        const int NO_PLAY = 0;
+
+       
         static void Main(string[] args)
         {
             //initializing local variable where no. of player is 1 and initial position is 0
@@ -21,9 +30,31 @@ namespace SnakeAndLadder
             //Generating DieRoll using Next method
             int dieRoll = random.Next(1, 7);
 
+            //Generating the player choice
+            int playerChoice = random.Next(0, 3);
+            
+
             Console.WriteLine("Welcome to Snake and Ladder World");
             Console.WriteLine("Player " + player + " is in position " + playerPos + " initially");
+            PlayerChoice(playerChoice);
             Console.Read();
+        }
+
+        //method check whether snake or ladder
+        public static void PlayerChoice(int choice)
+        {
+            switch(choice)
+            {
+                case LADDER:
+                    Console.WriteLine("Ladder move Forward");
+                    break;
+                case SNAKE:
+                    Console.WriteLine("Snake move Backward");
+                    break;
+                default:
+                    Console.WriteLine("Don't Move");
+                    break;
+            }
         }
 
         
