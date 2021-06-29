9c8aa45db4fde1a9a4d48aa15fa5d42d6ea98600
Jebakani <vjishwarya@gmail.com>
2021-06-29 12:42:02 +0530

    [JEBAKANI] UC4-Checking till win position

--- SnakeAndLadder/Program.cs
+++ SnakeAndLadder/Program.cs
@@ -17,44 +17,57 @@ namespace SnakeAndLadder
         const int LADDER = 2;
         const int SNAKE = 1;
         const int NO_PLAY = 0;
+        const int WIN = 100;
 
        
         static void Main(string[] args)
         {
+            Console.WriteLine("Welcome to Snake and Ladder World");
+
             //initializing local variable where no. of player is 1 and initial position is 0
             int player = 1;
             int playerPos = 0;
             //creating object for Random class
             Random random = new Random();
 
-            //Generating DieRoll using Next method
-            int dieRoll = random.Next(1, 7);
+            while (playerPos < WIN)
+            {
+                //Generating DieRoll using Next method
+                int dieRoll = random.Next(1, 7);
 
-            //Generating the player choice
-            int playerChoice = random.Next(0, 3);
+                //Generating the player choice
+                int playerChoice = random.Next(0, 3);
+
+                //current position of player
+                playerPos = PlayerChoice(playerChoice, playerPos, dieRoll);
+                Console.WriteLine("Player Position:" + playerPos);
+
+
+            }
+
             
-
-            Console.WriteLine("Welcome to Snake and Ladder World");
-            Console.WriteLine("Player " + player + " is in position " + playerPos + " initially");
-            PlayerChoice(playerChoice);
             Console.Read();
         }
 
         //method check whether snake or ladder
-        public static void PlayerChoice(int choice)
+        public static int PlayerChoice(int choice,int pos,int die)
         {
             switch(choice)
             {
                 case LADDER:
-                    Console.WriteLine("Ladder move Forward");
+                    pos += die;
                     break;
                 case SNAKE:
-                    Console.WriteLine("Snake move Backward");
+                    pos -= die;
                     break;
                 default:
-                    Console.WriteLine("Don't Move");
                     break;
             }
+            if(pos<0)
+            {
+                pos = 0;
+            }
+            return pos;
         }
 
         
