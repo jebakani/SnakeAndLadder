977a8afcc1f11d0af076ba4f6b5dd05391306b52
Jebakani <vjishwarya@gmail.com>
2021-06-29 11:59:43 +0530

    [JEBAKANI] UC1- Initializing player and initial position

--- SnakeAndLadder/Program.cs
+++ SnakeAndLadder/Program.cs
@@ -6,13 +6,17 @@ namespace SnakeAndLadder
     {
         /// <summary>
         /// Snake and Ladder computation
+        /// UC1-Initialize player and Initial position
         /// </summary>
         /// <param name="args">The arguments.</param>
         static void Main(string[] args)
         {
+            //initializing local variable where no. of player is 1 and initial position is 0
+            int player = 1;
+            int playerPos = 0;
             
             Console.WriteLine("Welcome to Snake and Ladder World");
-            
+            Console.WriteLine("Player " + player + " is in position " + playerPos + " initially");
             Console.Read();
         }
 
